27c5f596c4c8d89d76e64e91028c3441944d4064
Joseph Furlott <jmfurlott@gmail.com>
2012-10-23 20:33:05 -0400

    4.0 bug fix

--- src/com/example/droidbox/Main.java
+++ src/com/example/droidbox/Main.java
@@ -43,13 +43,13 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         songs = new SongList();
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
-        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
+//        songs.add(new Song("Title","Artist","Album", "4"));
     	try {
     		songs = getQueue();
 	
@@ -185,10 +185,11 @@ public class Main extends Activity {
     		catch (Exception m) {
     			try {
     				songs = getLibrary();
-    			} catch(Exception n) {
-    				Log.e("JSON Parser", "not connecting to data");
     				testFileWriter writeLibrary = new testFileWriter();
     	        	writeLibrary.update(this,songs);	
+    			} catch(Exception n) {
+    				Log.e("JSON Parser", "not connecting to data");
+
     			}
     			
     		}
